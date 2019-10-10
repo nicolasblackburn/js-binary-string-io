08a064753d76feedd7907c768e2fda2f2ef0cff1
Nicolas Blackburn <nicolas.blackburn.ca@gmail.com>
2019-10-09 23:22:43 -0400

    fixed signed number big endian

--- index.js
+++ index.js
@@ -1,89 +1,102 @@
 module.exports = (({fromCharCode}) => {
-    function read(fmt, bytes) {
-        const helper = (fmt, bytes) => {
-            if (fmt instanceof String) {
-                return [bytes.slice(0, fmt.size), bytes.slice(fmt.size)];
-            } else if (fmt instanceof Number) {
-                let value = 0;
-                let sign = 0;
-                for (let n = 0 ; n < fmt.size; n++) {
-                    const index = fmt.bigEndian ? n : fmt.size - n - 1;
-                    let byte = bytes.charCodeAt(index);
-                    if (0 === index && fmt.signed) {
-                        sign = 0b1000000 & byte;
-                        byte = 0b111111 & byte;
-                    }
-                    value += byte << (8 * (fmt.size - n - 1));
-                }
-    
-                if (fmt.signed && sign === 1) {
-                    value *= -1;
-                }
-    
-                return [value, bytes.slice(fmt.size)];
-                    
-            } else if (fmt instanceof Struct) {
-                const value = {};
-                let bytes2 = bytes;
-                for (const [key, fmt2] of Object.entries(fmt.props)) {
-                    const pair = helper(fmt2, bytes2);
-                    value[key] = pair[0];
-                    bytes2 = pair[1];
-                }
-                return [value, bytes2];
-            }
-        };
-        return helper(fmt, bytes)[0];
-    }
-
-    function write(fmt, data) {
-        const helper = (fmt, data, bytes) => {
-            if (fmt instanceof String) {
-                return bytes + data.padEnd(fmt.size, fromCharCode(0));
-            } else if (fmt instanceof Number) {
-                const byteMask = (1 << 8) - 1;
-                for (let n = 0 ; n < fmt.size; n++) {
-                    const shift = fmt.bigEndian ? fmt.size - n - 1 : n;
-                    let byte = data >> (shift * 8) & byteMask;
-                    if (fmt.size - 1 === shift && fmt.signed) {
-                        byte += 0b10000000;
-                    }
-                    bytes += fromCharCode(byte);
-                }
-                return bytes;
-                    
-            } else if (fmt instanceof Struct) {
-                for (const [key, fmt2] of Object.entries(fmt.props)) {
-                    bytes += helper(fmt2, data[key], "");
-                }
-                return bytes;
-            }
-        };
-        return helper(fmt, data, "");
-    }
-
-    function String(size) {
-        if (this instanceof String === false) {
-            return new String(size);
+  function read(fmt, bytes) {
+    const helper = (fmt, bytes) => {
+      if (fmt instanceof String) {
+        return [bytes.slice(0, fmt.size), 
+          bytes.slice(fmt.size)];
+      } else if (fmt instanceof Number) {
+        let value = 0;
+        let sign = 0;
+        for (let n = 0 ; n < fmt.size; n++) {
+          const index = fmt.bigEndian ? 
+            n : fmt.size - n - 1;
+          let byte = bytes.charCodeAt(index);
+          if (0 === index && fmt.signed) {
+            sign = 0b10000000 & byte;
+            byte = 0b1111111 & byte;
+          }
+          value += byte << 
+            (8 * (fmt.size - n - 1));
         }
-        this.size = size;
-    }
-
-    function Number(size, signed, bigEndian) {
-        if (this instanceof Number === false) {
-            return new Number(size, signed, bigEndian);
+        
+        if (fmt.signed && sign !== 0) {
+          value *= -1;
         }
-        this.size = size;
-        this.signed = signed;
-        this.bigEndian = bigEndian;
-    }
 
-    function Struct(props) {
-        if (this instanceof Struct === false) {
-            return new Struct(props);
+        return [value, bytes.slice(fmt.size)];
+
+      } else if (fmt instanceof Struct) {
+        const value = {};
+        let bytes2 = bytes;
+        for (const [key, fmt2] of 
+          Object.entries(fmt.props)) {
+          const pair = helper(fmt2, bytes2);
+          value[key] = pair[0];
+          bytes2 = pair[1];
         }
-        this.props = props;
-    }
+        return [value, bytes2];
+      }
+    };
+    return helper(fmt, bytes)[0];
+  }
 
-    return {read, write, String, Number, Struct};
+  function write(fmt, data) {
+    const helper = (fmt, data, bytes) => {
+      if (fmt instanceof String) {
+        return bytes + 
+          data.padEnd(fmt.size, 
+            fromCharCode(0));
+      } else if (fmt instanceof Number) {
+        const byteMask = (1 << 8) - 1;
+        const sign = data < 0 && fmt.signed;
+        if (data < 0) {
+          data *= -1;
+        }
+        for (let n = 0 ; n < fmt.size; n++) {
+          const shift = fmt.bigEndian ? 
+            fmt.size - n - 1 : n;
+          let byte = data >> (shift * 8) & 
+            byteMask;
+          if (fmt.size - 1 === shift && sign) {
+            byte += 0b10000000;
+          }
+          bytes += fromCharCode(byte);
+        }
+        return bytes;
+
+      } else if (fmt instanceof Struct) {
+        for (const [key, fmt2] of 
+          Object.entries(fmt.props)) {
+          bytes += helper(fmt2, data[key], "");
+        }
+        return bytes;
+      }
+    };
+    return helper(fmt, data, "");
+  }
+
+  function String(size) {
+    if (this instanceof String === false) {
+      return new String(size);
+    }
+    this.size = size;
+  }
+
+  function Number(size, signed, bigEndian) {
+    if (this instanceof Number === false) {
+      return new Number(size, signed, bigEndian);
+    }
+    this.size = size;
+    this.signed = signed;
+    this.bigEndian = bigEndian;
+  }
+
+  function Struct(props) {
+    if (this instanceof Struct === false) {
+      return new Struct(props);
+    }
+    this.props = props;
+  }
+
+  return {read, write, String, Number, Struct};
 })(String);
--- test.js
+++ test.js
@@ -2,54 +2,81 @@ const assert = require("assert");
 const bytes = require("./index.js");
 
 (() => {
-    const fmt = bytes.Struct({
-        name: bytes.String(10),
-        age: bytes.String(3),
-        book: bytes.Struct({
-            type: bytes.String(10),
-            title: bytes.String(10)
-        })
-    });
-    const data = {
-        name: "Nicolas".padEnd(10, " "),
-        age: "37".padEnd(3, " "),
-        book: {
-            type: "comics".padEnd(10, " "),
-            title: "Black Hole".padEnd(10, " ")
-        }
-    };
-    assert.equal(bytes.write(fmt, data), "Nicolas   37 comics    Black Hole");
-    assert.deepEqual(bytes.read(fmt, "Nicolas   37 comics    Black Hole"), data);
+  const fmt = bytes.Struct({
+    name: bytes.String(10),
+    age: bytes.String(3),
+    book: bytes.Struct({
+      type: bytes.String(10),
+      title: bytes.String(10)
+    })
+  });
+  const data = {
+    name: "Nicolas".padEnd(10, " "),
+    age: "37".padEnd(3, " "),
+    book: {
+      type: "comics".padEnd(10, " "),
+      title: "Black Hole".padEnd(10, " ")
+    }
+  };
+  assert.equal(bytes.write(fmt, data), 
+    "Nicolas   37 comics    Black Hole");
+  assert.deepEqual(
+    bytes.read(fmt, 
+               "Nicolas   37 comics    Black Hole"), 
+    data);
 })();
 
 (() => {
-    const fmt = bytes.Number(2, false, true);
-    const out = bytes.write(fmt, 10);
-    assert.equal(out.charCodeAt(0), 0);
-    assert.equal(out.charCodeAt(1), 10);
-    assert.equal(bytes.read(fmt, out), 10);
+  const fmt = bytes.Number(2, false, true);
+  const out = bytes.write(fmt, 10);
+  assert.equal(out.charCodeAt(0), 0);
+  assert.equal(out.charCodeAt(1), 10);
+  assert.equal(bytes.read(fmt, out), 10);
 })();
 
 (() => {
-    const fmt = bytes.Number(2, false, true);
-    const out = bytes.write(fmt, 1032);
-    assert.equal(out.charCodeAt(0), 4);
-    assert.equal(out.charCodeAt(1), 8);
-    assert.equal(bytes.read(fmt, out), 1032);
+  const fmt = bytes.Number(2, false, true);
+  const out = bytes.write(fmt, 1032);
+  assert.equal(out.charCodeAt(0), 4);
+  assert.equal(out.charCodeAt(1), 8);
+  assert.equal(bytes.read(fmt, out), 1032);
 })();
 
 (() => {
-    const fmt = bytes.Number(2, false, false);
-    const out = bytes.write(fmt, 10);
-    assert.equal(out.charCodeAt(1), 0);
-    assert.equal(out.charCodeAt(0), 10);
-    assert.equal(bytes.read(fmt, out), 10);
+  const fmt = bytes.Number(2, false, false);
+  const out = bytes.write(fmt, 10);
+  assert.equal(out.charCodeAt(1), 0);
+  assert.equal(out.charCodeAt(0), 10);
+  assert.equal(bytes.read(fmt, out), 10);
 })();
 
 (() => {
-    const fmt = bytes.Number(2, false, false);
-    const out = bytes.write(fmt, 1032);
-    assert.equal(out.charCodeAt(1), 4);
-    assert.equal(out.charCodeAt(0), 8);
-    assert.equal(bytes.read(fmt, out), 1032);
+  const fmt = bytes.Number(2, false, false);
+  const out = bytes.write(fmt, 1032);
+  assert.equal(out.charCodeAt(1), 4);
+  assert.equal(out.charCodeAt(0), 8);
+  assert.equal(bytes.read(fmt, out), 1032);
 })();
+
+(() => {
+  const n = 1032;
+  const fmt = bytes.Number(2, true, true);
+  const out = bytes.write(fmt, n);
+  assert.equal(out.charCodeAt(0), 4);
+  assert.equal(out.charCodeAt(1), 8);
+  assert.equal(bytes.read(fmt, out), n);
+})();
+
+(() => {
+  const {log} = console;
+  const n = -1032;
+  const fmt = bytes.Number(2, true, true);
+  const out = bytes.write(fmt, n);
+  //log((-1*n).toString(2).padStart(16, "0"));
+  //log(out.charCodeAt(0).toString(2).padStart(8, "0"));
+  //log(out.charCodeAt(1).toString(2).padStart(8, "0"))
+  assert.equal(out.charCodeAt(0), 132);
+  assert.equal(out.charCodeAt(1), 8);
+  assert.equal(bytes.read(fmt, out), n);
+})();
+
